ec55a67f0915131ef3a1cf55dca3c068c9a3b3e4
Kyle Bittinger <kylebittinger@gmail.com>
2010-11-05 18:49:17 +0000

    Added TaxonomyHeatmap function.

--- qiimer.R
+++ qiimer.R
@@ -1,11 +1,18 @@
-ReadMappingFile <- function(filepath) {
+ReadCommentedHeader <- function(fileobj) {
+  # Private function to read a (possibly) commented header line from a tsv file.
+  
+  header <- readLines(fileobj, n=1)
+  header <- sub("^#", "", header)
+  unlist(strsplit(header, "\t"))
+}
+
+ReadSampleMapping <- function(filepath) {
+  # Read a QIIME sample mapping file and return a data frame.
+  
   mapping.file <- file(filepath)
   # Automatically closed during the call to read.table.
 
-  # A "#" character may appear at start of header
-  header <- readLines(mapping.file, n=1)
-  header <- sub("^#", "", header)
-  cols <- unlist(strsplit(header, "\t"))
+  cols <- ReadCommentedHeader(mapping.file)
   t <- read.table(mapping.file, col.names=cols, sep="\t", quote="",
                   row.names=NULL,
                   na.strings=c("NA", "na", "Null", "null"))
@@ -16,6 +23,8 @@ ReadMappingFile <- function(filepath) {
 }
 
 ReadOtuTable <- function(filepath) {
+  # Read a QIIME OTU table and return a data frame.
+  
   otu.file <- file(filepath)
 
   # Often, header line is preceeded by a comment line.
@@ -27,12 +36,66 @@ ReadOtuTable <- function(filepath) {
   }
   cols <- unlist(strsplit(header, "\t"))
 
-  cols[1] <- "OtuID"
-  cols[length(cols)] <- "ConsensusLineage"
-
+  cols[1] <- "OTU"
+  cols <- sub("Consensus Lineage", "Lineage", cols)
+  
   t <- read.table(otu.file, col.names=cols, sep="\t", quote="",
                   row.names=NULL)
 
   t$OtuID <- as.character(t$OtuID)
   t
+}
+
+ReadAssignmentTable <- function(filepath) {
+  # Read a parsed table of OTU assignments
+
+  read.table(filepath, header=TRUE, sep="\t", quote="", row.names=NULL)
+}
+
+TaxonomyHeatmap <- function(otu.counts, otu.labels) {
+  # Requires a modified OTU table containing taxa classified by rank.
+  # This can be created by parse_taxonomy.py in the qiime_pipeline
+  # module.
+  #
+
+  breaks <- c(-1, 0, 0.002, 0.004, 0.006, 0.01, 0.03, 0.05, 0.07, 0.09, 0.2, 0.4,
+              0.6, 0.7, 0.9, 2)
+
+  # set up color table
+  colors <- c(rgb(255, 255, 255, max=255), rainbow(14))
+  colors[7] <- rgb(0, 204, 0, max=255)
+  colors[8] <- rgb(0, 255, 237, max=255)
+  colors[11] <- rgb(25, 0, 217, max=255)
+  colors[12] <- rgb(157, 0, 255, max=255)
+  colors[13] <- rgb(220, 91, 255, max=255)
+  colors[14] <- rgb(235, 182, 255, max=255)
+  
+
+  sample.labels <- names(otu.counts)
+  assignments <- aggregate(otu.counts, list(Label=otu.labels), sum)
+
+  assignment.labels <- assignments$Label
+  assignment.counts <- assignments[,sample.labels]
+  ConvertToProportion <- function(x) { x / sum(x) }
+  assignment.fracs <- sapply(assignment.counts, ConvertToProportion)
+
+
+  heatmap.2(as.matrix(assignment.fracs),
+
+            # dendrogram control
+            dendrogram="both",
+            Rowv=TRUE,
+            Colv=TRUE,
+            
+            scale="none",
+            labRow=assignment.labels,
+            breaks=breaks,
+            col=colors,
+            margins=c(5,20),
+            cexRow=1,
+            cexCol=1,
+            density.info="none",
+            trace="none",
+          )
+
 }
