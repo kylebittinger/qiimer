6ca5ff2c6cc1b08696a2b2f348a3993067d9f66b
Kyle Bittinger <kylebittinger@gmail.com>
2013-09-19 12:25:51 -0400

    Renamed parse_otu_table to read_qiime_otu_table, detect metadata column in OTU tables according to conventions in QIIME source code.  Counts table was returned as data frame despite the documentation, now returned as matrix.
    Improved interface to parse_assignments and renamed as split_assignments.
    
    Introduced new function, simplify_assignments, to create taxa labels for plots.  Eliminated prettyprint_assignments in favor of simplify_assignments.
    
    Function otu_heatmap now invisibly returns the matrix of data displayed in the plot, and has a flag to suppress plotting.
    
    Improved and simplified the implementation of the saturated_rainbow palette.
    
    Eliminated the otu_barplot function.
    
    Added tests for all functions listed above.

--- R/otu_table.R
+++ R/otu_table.R
@@ -1,4 +1,4 @@
-#' Parse a QIIME OTU table file.
+#' Parse a QIIME OTU table file in "calssic" format.
 #'
 #' @param filepath Path to OTU table file.
 #' @param commented TRUE if the header line is preceeded by an additional
@@ -6,11 +6,11 @@
 #'   tables generated with QIIME, so we default to TRUE.
 #' @return A list with four attributes: sample_ids, otu_ids, counts, and 
 #'   metadata, a data structure similar to that returned by the python 
-#'   function qiime.parse.parse_otu_table.  The sample_ids, otu_ids, and
+#'   function `qiime.parse.parse_otu_table`.  The sample_ids, otu_ids, and
 #'   metadata attributes are character vectors.  The counts attribute is an
 #'   integer matrix with one column per sample_id and one row per otu_id.
 #' @export
-parse_otu_table <- function(filepath, commented=TRUE) {
+read_qiime_otu_table <- function(filepath, commented=TRUE) {
   f <- file(filepath, "rt")
   header_line <- readLines(f, n=1)
   if (commented) {
@@ -18,52 +18,46 @@ parse_otu_table <- function(filepath, commented=TRUE) {
   }
   col_names <- unlist(strsplit(header_line, "\t"))
 
+  # Following the QIIME implementation, we decide if the table contains 
+  # metadata by inspecting the final column for "Consensus Lineage", "OTU
+  # Metadata", or "Taxonomy" (spaces and capitalization ignored)
+  has_metadata <- grepl(
+    "(Consensus ?Lineage)|(OTU ?Metadata)|(Taxonomy)",
+    col_names[length(col_names)],
+    ignore.case=T, perl=T)
+  
   col_classes <- rep("numeric", times=length(col_names))
-  col_classes[c(1, length(col_classes))] <- "character"
-
+  
+  if (has_metadata) {
+    col_classes[c(1, length(col_classes))] <- "character"
+  }
+  
   full_otu_table <- read.table(
     f, col.names=col_names, colClasses=col_classes, sep="\t", 
     quote="", as.is=TRUE, header=FALSE)
   close(f)
 
-  data_cols <- 2:(length(col_names) - 1)
+  data_cols <- if (has_metadata) {
+    2:(length(col_names) - 1) 
+  } else {
+    2:length(col_names)
+  } 
 
   sample_ids <- col_names[data_cols]
   otu_ids <- as.character(full_otu_table[,1])
 
-  counts <- full_otu_table[,data_cols]
+  counts <- as.matrix(full_otu_table[,data_cols])
   rownames(counts) <- otu_ids
 
-  metadata <- full_otu_table[,length(col_names)]
-  names(metadata) <- otu_ids
-
+  if (has_metadata) {
+    metadata <- as.character(full_otu_table[,length(col_names)])
+    names(metadata) <- otu_ids
+  } else {
+    metadata <- NULL
+  }
+    
   list(
     sample_ids=sample_ids, otu_ids=otu_ids, counts=counts, metadata=metadata)
-}
-
-#' Reformat taxonomic assignments for presentation.
-#'
-#' @param assignments A character vector of taxonomic assignments.
-#' @param sep The character separating taxa in each assignment.
-#' @param prefix_rank The rank of taxonomy to use as the first word in the
-#'   prettyprinted assignment.
-#' @param suffix_rank The rank of taxonomy to use as the second word in the
-#'   prettyprinted assignment.
-#' @return A character vector of reformatted assignments.
-#' @export
-prettyprint_assignments <- function(assignments, sep="; ", prefix_rank=2, suffix_rank=100) {
-  # make list of taxa
-  alltaxa <- strsplit(assignments, sep)
-  # Phylum and final classification
-  superraw.labels <- lapply(alltaxa, function(x) {
-    nx <- length(x)
-    if (nx >= suffix_rank) x[c(prefix_rank, suffix_rank)] else
-      if (nx > prefix_rank) x[c(prefix_rank, nx)] else x
-    })
-  # Paste these two items into a single string
-  raw.labels <- lapply(superraw.labels, paste, collapse=" ")
-  # Convert resulting nested list into a vector
-  unlist(raw.labels)
 }
 
 #' Standard taxonomic ranks
@@ -73,17 +67,17 @@ taxonomic_ranks <- c(
   "Domain", "Kingdom", "Phylum", "Class", "Order", 
   "Family", "Genus", "Species", "Strain")
 
-#' Parse taxonomic assignment strings
+#' Split taxonomic assignment strings
 #'
 #' @param assignments Character vector of taxonomic assignments.
-#' @param split Pattern on which to split taxa in assignment strings.
-#' @param ranks Character vector of taxonomic ranks, used as row names in the
+#' @param ranks Character vector of taxonomic ranks, used as column names in the
 #'   resultant data frame.
+#' @param split Pattern on which to split taxa in assignment strings.
 #' @param ... Additional parameters are passed to the \code{strsplit} function.
 #' @return A data frame of taxonomic assignments.
 #' @export
-parse_assignments <- function(
-  assignments, split="; ", ranks=taxonomic_ranks[2:8], ...) {
+split_assignments <- function(assignments, ranks=taxonomic_ranks[2:8], 
+  split="; ", ...) {
   a <- strsplit(as.character(assignments), split, ...)
   max_ranks <- max(sapply(a, length))
   a <- lapply(a, function (x) {
@@ -92,7 +86,35 @@ parse_assignments <- function(
   })
   a <- as.data.frame(do.call(rbind, a))
   colnames(a) <- ranks[1:ncol(a)]
+  if (!is.null(names(assignments))) {
+    rownames(a) <- names(assignments)
+  }
   a
+}
+
+#' Reformat taxonomic assignments for presentation.
+#'
+#' @param assignments_df A data frame of taxonomic assignments.
+#' @param rank1 The rank of taxonomy to use as the first word in the label.
+#' @param rank2 The rank of taxonomy to use as the second word in the label.
+#' @return A character vector of reformatted assignment labels.
+#' @export
+simplify_assignments <- function(assignments_df, rank1="Phylum", rank2="Genus") {
+  if (is.character(rank1)) {
+    rank1 <- match(rank1, colnames(assignments_df))
+  }
+  if (is.character(rank2)) {
+    rank2 <- match(rank2, colnames(assignments_df))
+  }
+  apply(assignments_df, 1, function (x) {
+    x <- na.omit(as.character(x))
+    n <- length(x)
+    if (n == 1)     return(x)
+    if (n < rank1)  return(paste(x, collapse=" "))
+    if (n == rank1) return(x[rank1])
+    if (n < rank2)  return(paste(x[rank1], x[length(x)]))
+    return(paste(x[rank1], x[rank2]))
+  })
 }
 
 #' Create a heatmap of OTU counts.
@@ -102,27 +124,33 @@ parse_assignments <- function(
 #' @param assignments A character vector of OTU assignments.  Length should
 #'   match number of rows in otu_counts.
 #' @param threshold Minimum number of OTU counts necessary for an assignment to
-#'   be included in the heatmap.  Assignment groups are filtered prior to
-#'   calculating the proportions, so these groups are effectively removed from
-#'   the analysis.
+#'   be included in the heatmap.  Assignments are filtered after calculating
+#'   the proportions, so the threshold setting does not affect the display of
+#'   the remaining OTUs.
+#' @param plot If true, display a plot.  If false, just return the computed
+#'   abundances.
 #' @param color Vector of colors to use in the heatmap.
-#' @param breaks Vector of color breaks, one element greater in length than `colors`.
+#' @param breaks Vector of color breaks, one element greater in length than
+#'   `colors`.
 #' @param ... Additional arguments are passed to the pheatmap function.
-#' @return A heatmap plot of the proportions of assignments in each sample
+#' @return A heatmap plot of the proportions of assignments in each sample,
+#'   and invisibly returns a matrix of the proportions in the plot.
 #' @export
-otu_heatmap <- function(otu_counts, assignments, threshold=0,
+otu_heatmap <- function(otu_counts, assignments, threshold=0, plot=T,
   color=saturated_rainbow(max(colSums(otu_counts))),
   breaks=seq(0, 1, length.out=length(color) + 1), ...) {
-  # The rowsum() function does not play well with factors, convert to character.
+  # rowsum() does not play well with factors
   assignments <- as.character(assignments)
-  # NA values in assignments will work fine but produce a warning from rowsum().
-  # However, NA values work well for representing unassigned taxa.  We therefore
-  # suppress warnings from this function.
+  # NA values in assignments work fine, but produce an unnecessary warning
   assignment_counts <- suppressWarnings(rowsum(otu_counts, assignments))
   rows_to_keep <- apply(assignment_counts, 1, sum) >= threshold
   assignment_fracs <- apply(assignment_counts, 2, function (x) {x / sum(x)}) 
-  assignment_fracs <- assignment_fracs[rows_to_keep,]
-  pheatmap(as.matrix(assignment_fracs), color=color, breaks=breaks, ...)
+  assignment_fracs <- as.matrix(assignment_fracs[rows_to_keep,])
+  if (plot) {
+    pheatmap(assignment_fracs, color=color, breaks=breaks, ...)
+  }
+  # Return underlying data
+  invisible(assignment_fracs)
 }
 
 #' Saturated rainbow palette
@@ -134,52 +162,10 @@ otu_heatmap <- function(otu_counts, assignments, threshold=0,
 #' @export
 saturated_rainbow <- function (n, saturation_limit=0.4) {
   saturated_len <- floor(n * (1 - saturation_limit))
-  unsaturated_len <- n - saturated_len - 2
-  
-  rainbow_colors <- rev(rainbow(unsaturated_len, start=0, end=0.6))
-  
-  first_color <- rainbow_colors[1]
-  presence_colors <- colorRampPalette(c("#FFFFFFFF", first_color))(2)
-  
-  last_color <- rainbow_colors[length(rainbow_colors)]
+  rainbow_colors <- rev(rainbow(n - saturated_len, start=0, end=0.6))
+  last_color <- tail(rainbow_colors, n=1)
   saturated_colors <- rep(last_color, saturated_len)
-  
-  c(presence_colors, rainbow_colors, saturated_colors)
-}
-
-#' Create a barplot of OTU assignments.
-#'
-#' @param otu_counts Matrix of OTU counts, one row per OTU and one column 
-#'   per sample
-#' @param assignments A character vector of OTU assignments.  Length should
-#'   match number of rows in otu_counts.
-#' @param max_categories  Maximum number of different assignments to be 
-#'   displayed in the plot.  The top assignments are determined by total count, 
-#'   and others are labeled "Other."  The default, 8, is the maximum supported 
-#'   by palettes in color brewer.
-#' @return A bar plot of OTU assignments.
-#' @export
-otu_barplot <- function(otu_counts, assignments, max_categories=8) {
-  assignment_counts <- rowsum(otu_counts, assignments)
-  assignment_labels <- rownames(assignment_counts)
-  if (length(assignments) > max_categories) {
-    assignment_order <- order(apply(assignment_counts, 1, sum), decreasing=TRUE)
-    top_assignments <- assignment_order[1:max_categories]
-    assignment_labels[-top_assignments] <- "Other"
-    assignment_counts <- rowsum(assignment_counts, assignment_labels)
-    assignment_labels <- rownames(assignment_counts)
-  }
-  melted_counts <- melt(
-    data.frame(Assignment=assignment_labels, assignment_counts),
-    variable_name="sample_id")
-  ggplot(melted_counts, aes(x=sample_id, y=value, fill=Assignment)) + 
-    geom_bar(position="fill") + 
-    theme_bw() + 
-    scale_x_discrete(name="", expand=c(0, 0)) + 
-    scale_y_continuous(name="Percent composition", formatter="percent", expand=c(0, 0)) +
-    opts(
-      axis.text.x=(theme_text(angle=90, hjust=1)), 
-      panel.grid.major=theme_blank(),
-      panel.grid_minor=theme_blank(),
-      panel.border=theme_blank())
+  colors <- c(rainbow_colors, saturated_colors)
+  colors[1] <- "#FFFFFFFF"
+  colors
 }
