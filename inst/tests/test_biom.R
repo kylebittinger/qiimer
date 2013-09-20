6dbd2c4bfd41d1b588f1b1e2d888f5c550e69a2e
Kyle Bittinger <kylebittinger@gmail.com>
2013-09-20 13:38:37 -0400

    Require biom package for relevant tests.

--- inst/tests/test_biom.R
+++ inst/tests/test_biom.R
@@ -1,47 +1,52 @@
+
+
 otu_ids <- c("0", "1", "2", "3", "5")
 sample_ids <- c("A.1", "A.2", "B.1", "B.2", "C.1")
 
-context('biom_raw_data, dense format')
+if (suppressMessages(require(biom))) {
 
-db <- read_biom(
-  system.file("testdata", "otu_table_dense.biom", package="qiimer"))
+  context('biom_raw_data, dense format')
 
-test_that("OTU IDs are parsed correctly", {
-  expect_equal(rownames(biom_raw_data(db)), otu_ids)
-})
+  db <- read_biom(
+    system.file("testdata", "otu_table_dense.biom", package="qiimer"))
 
-test_that("Sequence IDs are parsed correctly", {
-  expect_equal(colnames(biom_raw_data(db)), sample_ids)
-})
+  test_that("OTU IDs are parsed correctly", {
+    expect_equal(rownames(biom_raw_data(db)), otu_ids)
+  })
 
-test_that("OTU counts are correct", {
-  expect_equal(
-    biom_raw_data(db)["0",], 
-    structure(c(5, 10, 2, 0, 3), names=sample_ids))
-  expect_equal(
-    biom_raw_data(db)[,"A.1"], 
-    structure(c(5, 76, 2, 637, 0), names=otu_ids))
-})
+  test_that("Sequence IDs are parsed correctly", {
+    expect_equal(colnames(biom_raw_data(db)), sample_ids)
+  })
 
-context('biom_raw_data, sparse format')
+  test_that("OTU counts are correct", {
+    expect_equal(
+      biom_raw_data(db)["0",], 
+      structure(c(5, 10, 2, 0, 3), names=sample_ids))
+    expect_equal(
+      biom_raw_data(db)[,"A.1"], 
+      structure(c(5, 76, 2, 637, 0), names=otu_ids))
+  })
 
-sb <- read_biom(
-  system.file("testdata", "otu_table_sparse.biom", package="qiimer"))
+  context('biom_raw_data, sparse format')
 
-test_that("OTU IDs are present", {
-  expect_equal(levels(biom_raw_data(sb)$OTU), otu_ids)
-})
+  sb <- read_biom(
+    system.file("testdata", "otu_table_sparse.biom", package="qiimer"))
 
-test_that("Sequence IDs are present", {
-  expect_equal(levels(biom_raw_data(sb)$SampleID), sample_ids)
-})
+  test_that("OTU IDs are present", {
+    expect_equal(levels(biom_raw_data(sb)$OTU), otu_ids)
+  })
 
-test_that("OTU counts are in the same order as the biom file", {
-  expect_equal(
-    biom_raw_data(sb)$Reads, 
-    c(5, 10, 2, 3, 76, 23, 28, 43, 56, 2, 1, 637, 61, 63, 77, 34, 1))
-})
+  test_that("Sequence IDs are present", {
+    expect_equal(levels(biom_raw_data(sb)$SampleID), sample_ids)
+  })
 
+  test_that("OTU counts are in the same order as the biom file", {
+    expect_equal(
+      biom_raw_data(sb)$Reads, 
+      c(5, 10, 2, 3, 76, 23, 28, 43, 56, 2, 1, 637, 61, 63, 77, 34, 1))
+  })
+
+}
 context('biom_taxonomy')
 
 test_that("Taxonomy is correct", {
