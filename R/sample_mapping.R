5619c1465d7601512b8d55c8574cb87d1f3c65e0
Anatoly Dryga <anatolydryga@gmail.com>
2014-12-16 14:58:43 -0500

    sanity checks for failures in qiime mapping file: 1. if file does not exist 2. SampleIDs should be unique 3. All (but Description) columns should be non-empty(explicit NA required).

--- R/sample_mapping.R
+++ R/sample_mapping.R
@@ -8,6 +8,9 @@
 #'   is returned as a character vector.
 #' @export
 read_qiime_mapping_file <- function(filepath) {  
+  if ( ! file.exists(filepath)) {
+    stop(paste("File", filepath, "does not exist. Check the name or location of the file."));
+  }
   sample_file <- file(filepath, 'rt')
 
   header <- readLines(sample_file, n=1)
@@ -26,6 +29,9 @@ read_qiime_mapping_file <- function(filepath) {
   # The SampleID column is often used to extract data from distance matrices 
   # and OTU tables.  Storing as a character vector facilitates this practice.
   sample_data$SampleID <- as.character(sample_data$SampleID)
+  if ( ! is_unique_ids(sample_data$SampleID)) {
+    stop(paste("File", filepath, "has duplicate SampleIDs. Check that SampleIDs are unique."))
+  }
   
   # Ideally, the Description column should contain a unique free text
   # description of each sample.  In this case, there is no advantage to using
@@ -34,5 +40,40 @@ read_qiime_mapping_file <- function(filepath) {
     sample_data$Description <- as.character(sample_data$Description)
   }
 
+  invalid_columns <- column_with_empty_value(filepath, cols)
+  if ( ! is.null(invalid_columns)) {
+    stop(paste("Column(s)", invalid_columns, "have empty entry. Write explicitly NA or value needed."))
+  }
+
   sample_data
 } 
+
+is_unique_ids <- function(data) {
+    ids <- unlist(data)
+    ids <- ids[! is.na(ids)]
+    
+    len <- length(ids)
+    len_uniq <- length(unique(ids))
+
+    (len == len_uniq)
+}
+
+# no implicit empty elements, but have to read file again
+column_with_empty_value <- function(qiime_map_file, column_names, dont_check=c("Description")) {
+    qiime_map <- read.table(
+      qiime_map_file,
+      col.names=column_names,
+      sep="\t",
+      quote="",
+      row.names=NULL,
+      colClasses="character",
+      na.strings=c("NA", "na", "Null", "null")
+    )
+    columns_to_check <- setdiff(column_names, dont_check)
+    is_valid_column <- sapply(columns_to_check, function(column) all(qiime_map[[column]] != ""))
+    invalid_columns <- columns_to_check[ ! is_valid_column]
+    if (length(invalid_columns) > 0) {
+        return(invalid_columns)
+    } 
+    NULL
+}
